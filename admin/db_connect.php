e3b07d82f5f583f42b3351c2cfeaf8d637c60688
AbdelrhmanAmrAli <76621761+AbdelrhmanAmrAli@users.noreply.github.com>
2022-01-02 15:22:38 +0200

    complete signIn and signUp

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,4 +1,4 @@
 <?php
 include 'database/initialize_db.php';
 
-$conn= new mysqli('localhost','root','mysql','theater_db')or die("Could not connect to mysql".mysqli_error($conn));
+$conn= new mysqli('localhost','root','','theater_db')or die("Could not connect to mysql".mysqli_error($conn));
