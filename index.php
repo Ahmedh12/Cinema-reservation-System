e00d48b8d9388eb1a0846abafefbc3d33376db0a
ahmed hussien <ahmedhussien783@gmail.com>
2022-01-05 20:18:20 +0200

    Fix Status Bar buttons according to being logged in or a guest

--- index.php
+++ index.php
@@ -21,9 +21,13 @@
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=home">Home</a></li>
-                    <!--<li class="nav-item"><a class="nav-link" href="index.php?page=movies">Movies</a></li>-->
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=login">User</a></li>
+                    <?php
+                    if (!isset($_COOKIE["LoggedIn"])) {
+                        echo '<li class="nav-item"><a class="nav-link" href="index.php?page=login">Login/Signup</a></li>';
+                        //echo '<li class="nav-item"><a class="nav-link" href="index.php?page=home">Home</a></li>';
+                    }
+                    ?>
+
                 </ul>
             </div>
         </div>
--- movie_carousel.php
+++ movie_carousel.php
@@ -1,10 +1,12 @@
-<?php 
-  include 'admin/db_connect.php';
-  $movies = $conn->query("SELECT * FROM movies  limit 10");
+<?php
+include 'admin/db_connect.php';
+$movies = $conn->query("SELECT * FROM movies  limit 10");
 ?>
 
-     
-                  <center><h3 class="text-primary">Now Showing</h3></center>
+
+<center>
+  <h3 class="text-primary">Now Showing</h3>
+</center>
 
 <div id="movie-carousel-field">
 
@@ -12,34 +14,32 @@
     <a href="javascript:void(0)" class="text"><i class="fa fa-angle-left"></i></a>
   </div>
   <div class="list">
-    <?php while($row=$movies->fetch_assoc()): ?>
-        <div class="movie-item">
-          <img class="" src="assets/img/<?php echo $row['poster_image']  ?>" alt="<?php echo $row['title'] ?>" >
-          <div class="mov-det">
-            <button type="button" class="btn btn-primary" data-id="<?php echo $row['id'].$row['room'] ?>">Reserve Seat</button>
-          </div>
+    <?php while ($row = $movies->fetch_assoc()) : ?>
+      <div class="movie-item">
+        <img class="" src="assets/img/<?php echo $row['poster_image']  ?>" alt="<?php echo $row['title'] ?>">
+        <div class="mov-det">
+          <button type="button" class="btn btn-primary" data-id="<?php echo $row['id'] . $row['room'] ?>">Reserve Seat</button>
         </div>
+      </div>
     <?php endwhile; ?>
   </div>
-   <div class="list-next list-nav">
+  <div class="list-next list-nav">
     <a href="javascript:void(0)" class="text"><i class="fa fa-angle-right"></i></a>
   </div>
 </div>
 
 <script>
-  
-  $('#movie-carousel-field .list-next').click(function(){
+  $('#movie-carousel-field .list-next').click(function() {
     $('#movie-carousel-field .list').animate({
-    scrollLeft:$('#movie-carousel-field .list').scrollLeft() + ($('#movie-carousel-field .list').find('img').width() * 3)
-   }, 'slow');
+      scrollLeft: $('#movie-carousel-field .list').scrollLeft() + ($('#movie-carousel-field .list').find('img').width() * 3)
+    }, 'slow');
   })
-  $('#movie-carousel-field .list-prev').click(function(){
+  $('#movie-carousel-field .list-prev').click(function() {
     $('#movie-carousel-field .list').animate({
-    scrollLeft:$('#movie-carousel-field .list').scrollLeft() - ($('#movie-carousel-field .list').find('img').width() * 3)
-   }, 'slow');
+      scrollLeft: $('#movie-carousel-field .list').scrollLeft() - ($('#movie-carousel-field .list').find('img').width() * 3)
+    }, 'slow');
   })
-  $('.mov-det button').click(function(){
-    location.replace('index.php?page=hall1&id='+$(this).attr('data-id'));
-    location.replace('index.php?page=hall'+$(this).attr('data-id')[1]+'&id='+$(this).attr('data-id')[0])
+  $('.mov-det button').click(function() {
+    location.replace('index.php?page=hall' + $(this).attr('data-id')[1] + '&id=' + $(this).attr('data-id')[0])
   })
 </script>
